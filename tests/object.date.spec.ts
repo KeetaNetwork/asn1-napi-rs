6256c1722454f430569245d9c429f537c4f23a60
Lucas Rosa <lrosa@keeta.com>
2024-11-06 16:26:42 -0300

    cleanup

--- tests/object.date.spec.ts
+++ tests/object.date.spec.ts
@@ -6,28 +6,33 @@ import * as lib from '..';
 
 
 const TEST_DATES: { in: lib.ASN1Date; out?: Date }[] = [
-    { in: { type: 'date', kind: 'utc', date: new Date(1729868125000) }, out: new Date(1729868125000) }, // 2024-10-25T14:55:25.000Z
-	{ in: { type: 'date', kind: 'utc', date: new Date(1729868125001) }, out: new Date(1729868125000) }, // 2024-10-25T14:55:25.000Z
+    { in: { type: 'date', kind: 'utc', date: new Date(1729868125000) }, out: new Date(1729868125000) },
+	{ in: { type: 'date', kind: 'utc', date: new Date(1729868125001) }, out: new Date(1729868125000) },
 	
-    { in: { type: 'date', kind: 'general', date: new Date(1729868125001) } }, // { object: 2024-10-25T14:55:25.001Z }
-	{ in: { type: 'date', kind: 'general', date: new Date(2524694400001) }, out: new Date(2524694400001) }, // 2050-01-01T00:00:00.001Z
+    { in: { type: 'date', kind: 'general', date: new Date(1729868125001) } },
+	{ in: { type: 'date', kind: 'general', date: new Date(2524694400001) }, out: new Date(2524694400001) },
     
-    { in: { type: 'date', kind: 'default', date: new Date(1729868125000) }, out: new Date(1729868125000) }, // 2024-10-25T14:55:25.000Z
-	{ in: { type: 'date', kind: 'default', date: new Date(2524694400001) }, out: new Date(2524694400001) }, // 2050-01-01T00:00:00.001Z
-    { in: { type: 'date', date: new Date(1729868125000) }, out: new Date(1729868125000) }, // 2024-10-25T14:55:25.000Z
-	{ in: { type: 'date', date: new Date(2524694400001) }, out: new Date(2524694400001) }, // 2050-01-01T00:00:00.001Z
+    { in: { type: 'date', kind: 'default', date: new Date(1729868125000) }, out: new Date(1729868125000) },
+	{ in: { type: 'date', kind: 'default', date: new Date(2524694400001) }, out: new Date(2524694400001) },
+    { in: { type: 'date', date: new Date(1729868125000) }, out: new Date(1729868125000) },
+	{ in: { type: 'date', date: new Date(2524694400001) }, out: new Date(2524694400001) },
 ]
+
+const DATE_UTC_ASN1 = new Uint8Array([
+    0x17, 0x0d, 0x32, 0x34, 0x31, 0x30, 0x32, 0x35, 0x31, 0x34,
+    0x35, 0x35, 0x32, 0x35, 0x5a
+]).buffer
+
+const DATE_GENERAL_ASN1 = new Uint8Array([
+    0x18, 0x13, 0x32, 0x30, 0x35, 0x30, 0x30, 0x31, 0x30, 0x32,
+    0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x2e, 0x30, 0x30, 0x31,
+    0x5a,
+]).buffer
 
 const TEST_DATES_ASN1 = [
     // UTC
-    new Uint8Array([
-        0x17, 0x0d, 0x32, 0x34, 0x31, 0x30, 0x32, 0x35, 0x31, 0x34,
-        0x35, 0x35, 0x32, 0x35, 0x5a
-    ]).buffer,
-    new Uint8Array([
-        0x17, 0x0d, 0x32, 0x34, 0x31, 0x30, 0x32, 0x35, 0x31, 0x34,
-        0x35, 0x35, 0x32, 0x35, 0x5a
-    ]).buffer,
+    DATE_UTC_ASN1,
+    DATE_UTC_ASN1,
 
     // GENERAL
     new Uint8Array([
@@ -35,31 +40,13 @@ const TEST_DATES_ASN1 = [
         0x31, 0x34, 0x35, 0x35, 0x32, 0x35, 0x2e, 0x30, 0x30, 0x31,
         0x5a,
     ]).buffer,
-    new Uint8Array([
-        0x18, 0x13, 0x32, 0x30, 0x35, 0x30, 0x30, 0x31, 0x30, 0x32,
-        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x2e, 0x30, 0x30, 0x31,
-        0x5a,
-    ]).buffer,
+    DATE_GENERAL_ASN1,
     
     // DEFAULT
-    new Uint8Array([
-        0x17, 0x0d, 0x32, 0x34, 0x31, 0x30, 0x32, 0x35, 0x31, 0x34,
-        0x35, 0x35, 0x32, 0x35, 0x5a
-    ]).buffer,
-    new Uint8Array([
-        0x18, 0x13, 0x32, 0x30, 0x35, 0x30, 0x30, 0x31, 0x30, 0x32,
-        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x2e, 0x30, 0x30, 0x31,
-        0x5a,
-    ]).buffer,
-    new Uint8Array([
-        0x17, 0x0d, 0x32, 0x34, 0x31, 0x30, 0x32, 0x35, 0x31, 0x34,
-        0x35, 0x35, 0x32, 0x35, 0x5a
-    ]).buffer,
-    new Uint8Array([
-        0x18, 0x13, 0x32, 0x30, 0x35, 0x30, 0x30, 0x31, 0x30, 0x32,
-        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x2e, 0x30, 0x30, 0x31,
-        0x5a,
-    ]).buffer,
+    DATE_UTC_ASN1,
+    DATE_GENERAL_ASN1,
+    DATE_UTC_ASN1,
+    DATE_GENERAL_ASN1,
 ]
 
 test('JS ASN1Date to ASN1 conversion', (t) => {
@@ -78,9 +65,9 @@ test('ASN1 to Js ASN1Date conversion from byte code', (t) => {
 
 test('ASN1 to ASN1Date conversion from base64', (t) => {
 	const obj = lib.ASN1Decoder.fromBase64(
-		'GBMyMDUwMDEwMjAwMDAwMC4wMDBa',
+		'GBMyMDI0MTAyNTE0NTUyNS4wMDFa',
 	)
-	t.deepEqual(obj.intoDate(), new Date(2524694400000))
+	t.deepEqual(obj.intoDate(), new Date(1729868125001))
 })
 
 test('ASN1 to Js ANS1Date conversion round trip', (t) => {
--- tests/object.string.spec.ts
+++ tests/object.string.spec.ts
@@ -27,31 +27,19 @@ test('JS ASN1String to ASN1 conversion', (t) => {
 
 test('ASN1 to Js ASN1String conversion from byte code', (t) => {
 	TEST_STRINGS_ASN1.forEach((v, i) => {
-        // const obj = new lib.ASN1Decoder(v)
-		// t.deepEqual(obj.intoString(), TEST_STRINGS[i].out ?? TEST_STRINGS[i].in.value)
+        const obj = new lib.ASN1Decoder(v)
+		t.deepEqual(obj.intoString(), TEST_STRINGS[i].out ?? TEST_STRINGS[i].in.value)
 		t.deepEqual(lib.ASN1toJS(v), TEST_STRINGS[i].out ?? TEST_STRINGS[i].in)
 	})
 })
 
-// test('ASN1 to ASN1String conversion from base64', (t) => {
-// 	const obj = lib.ASN1Decoder.fromBase64(
-// 		'A0cAMEQCIHmXWc+ASZ/3agYiWczwPR7JkRbJSAdL26s0/YPyHb0/AiBUOWxP9BGZG9oPX6LEaJ3WNQjqCF/Yk69p3x37srdqAA==',
-// 	)
+test('ASN1 to ASN1String conversion from base64', (t) => {
+	const obj = lib.ASN1Decoder.fromBase64(
+		'DAZUZXPhhLM=',
+	)
 
-// 	t.deepEqual(obj.intoBitString(), {
-// 		type: 'bitstring',
-// 		value: Buffer.from(
-// 			new Uint8Array([
-// 				0x30, 0x44, 0x02, 0x20, 0x79, 0x97, 0x59, 0xcf, 0x80, 0x49, 0x9f, 0xf7,
-// 				0x6a, 0x06, 0x22, 0x59, 0xcc, 0xf0, 0x3d, 0x1e, 0xc9, 0x91, 0x16, 0xc9,
-// 				0x48, 0x07, 0x4b, 0xdb, 0xab, 0x34, 0xfd, 0x83, 0xf2, 0x1d, 0xbd, 0x3f,
-// 				0x02, 0x20, 0x54, 0x39, 0x6c, 0x4f, 0xf4, 0x11, 0x99, 0x1b, 0xda, 0x0f,
-// 				0x5f, 0xa2, 0xc4, 0x68, 0x9d, 0xd6, 0x35, 0x08, 0xea, 0x08, 0x5f, 0xd8,
-// 				0x93, 0xaf, 0x69, 0xdf, 0x1d, 0xfb, 0xb2, 0xb7, 0x6a, 0x00,
-// 			]),
-// 		),
-// 	})
-// })
+	t.deepEqual(obj.intoString(), 'Tes\u1133')
+})
 
 test('ASN1 to Js ANS1String conversion round trip', (t) => {
 	TEST_STRINGS_ASN1.forEach((v, i) => {
